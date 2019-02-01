a912168013aedd78c6afbc71986388665e9e5cbc
pougetb <38034238+pougetb@users.noreply.github.com>
2019-02-01 18:19:18 +0100

    Creation d'une trajectoire depuis JSON (sans le fichier)

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
 var map = L.map('map');
-map.setView([43.6326, 3.87], 16);
+map.setView([41.147519, -8.610814], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
+let myJSON = '{"DonneesBrut" : {"1" : {"positions" : [{"x":41.154473,"y":-8.644918,"date":"01/01/01"},{"x":41.166420,"y":-8.643001,"date":"02/01/01"},{"x":41.172972,"y":-8.625212,"date":"03/01/01"}]},"2" : {"positions" : [{"x":41.172103,"y":-8.605885,"date":"04/01/01"},{"x":41.162420,"y":-8.607168,"date":"05/01/01"},{"x":41.153670,"y":-8.604588,"date":"06/01/01"}]},"3" : {"positions" : [{"x":41.149048,"y":-8.585653,"date":"07/01/01"},{"x":41.156342,"y":-8.594473,"date":"08/01/01"},{"x":41.153428,"y":-8.610291,"date":"09/01/01"},{"x":41.155698,"y":-8.621481,"date":"10/01/01"}]}}}';
+
+
+let myNewJSON = JSON.parse(myJSON);
+console.log(myNewJSON);
+//console.log(myNewJSON);
+var latlngs = new Array();
+
+var i=1;
+
+while(1){console.log(i + "   " + myNewJSON.DonneesBrut[i]);
+	if (typeof myNewJSON.DonneesBrut[i] === 'undefined'){
+		break;
+	}
+	latlngs[i-1] = new Array();
+	for(var j=0 ; j<myNewJSON.DonneesBrut[i].positions.length ; j++){
+		latlngs[i-1][j] = [myNewJSON.DonneesBrut[i].positions[j].x, myNewJSON.DonneesBrut[i].positions[j].y];
+	}
+
+	i++;
+}
+
+
+
+console.log(latlngs);
+
+var polyline = L.polyline(latlngs, {color: 'blue'}).addTo(map);
+
