47fb69b4045b23e21589e18690d1c9599f618b9f
simsim <gilotsimon@gmail.com>
2019-04-28 15:49:51 +0200

    fix creation/init page help
    les onglets étaient toujours actifs après avoir cliqué sur la page help

--- script.js
+++ script.js
@@ -1017,6 +1017,11 @@ $(".nav_fullscreen").on("click",function(p_this){
 $(".onglet_options").filter("[attr_type=help]").on("click",function(){
 	let tab_page_content = $(".tab-content>.tab-pane");
 
+	//on retire les class active des onglets
+	let tab_onglet_fullscreen = $(".onglet_fullscreen");
+	for(let onglet in tab_onglet_fullscreen){
+		$(tab_onglet_fullscreen[onglet]).removeClass("active");
+	}
 	//on cache toute les pages
 	for(let i=0;i<tab_page_content.length;i++){
 		$(tab_page_content[i]).hide();
